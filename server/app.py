730fe9565c5ecdd187000ce1ad7a82c7b64d7710
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-03-03 11:09:21 +0300

    done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,65 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
-
-@app.route('/messages/<int:id>')
+    
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        
+        messages_list = [message.to_dict() for message in messages]
+        return jsonify(messages_list), 200
+    
+    elif request.method == 'POST':
+        data=request.json
+        username = data.get("username")
+        body = data.get("body")
+        
+        if username is None or body is None:
+            return jsonify({"error": "Missing required fields"}), 400
+        
+        new_message = Message(username=username,body=body)
+        
+        db.session.add(new_message)
+        db.session.commit()
+        
+        return jsonify(new_message.to_dict()), 201
+        
+@app.route('/messages/<int:id>', methods=['GET','PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    
+    if request.method == 'GET':
+        message = Message.query.filter_by(id=id).first()
+        
+        if not message:
+            return jsonify({"error": "Message not found"}), 404
 
+        return jsonify(message.to_dict()), 200
+    
+    elif request.method == 'PATCH':
+        message = Message.query.filter_by(id=id).first()
+
+        if not message:
+            return jsonify({"error": "Message not found"}), 404
+        
+        data = request.json
+        for attr, value in data.items():
+            setattr(message, attr, value)
+        
+        db.session.commit()
+        
+        return jsonify(message.to_dict()), 200
+    
+    elif request.method == 'DELETE':
+        message = Message.query.filter_by(id=id).first()
+
+        if not message:
+            return jsonify({"error": "Message not found"}), 404
+
+        db.session.delete(message)
+        db.session.commit()
+        
+        return jsonify({"deleted":"Message deleted succesfully"})            
+    
 if __name__ == '__main__':
     app.run(port=5555)
